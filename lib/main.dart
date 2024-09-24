7f053c5f4a8655ac4960c343da91de58ef3139c2
AliA5y <ali.route9@gmail.com>
2024-09-24 11:01:25 +0300

    fix: add dependency of sqflite, and required initiallization code to let the app work in windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_bloc/data/repositories/home_repository.dart';
 import 'package:notes_bloc/views/home_view.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'blocs/home/home_bloc.dart';
 
 void main() {
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+  }
+  // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
+  // this step, it will use the sqlite version available on the system.
+  databaseFactory = databaseFactoryFfi;
   runApp(const MyApp());
 }
 
