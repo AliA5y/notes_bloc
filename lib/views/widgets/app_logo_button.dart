3455ef330cfd9bb0bdcfaff27041ef28dd615a04
AliA5y <ali.route9@gmail.com>
2024-10-05 10:59:27 +0300

    refactor: refactor app logo button

--- lib/views/widgets/app_logo_button.dart
+++ lib/views/widgets/app_logo_button.dart
@@ -8,43 +8,46 @@ class AppLogoButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            // spreadRadius: 1,
-            color: Theme.of(context).colorScheme.inverseSurface.withAlpha(20),
-            offset: const Offset(-1, -1),
-            blurRadius: 4,
-          ),
-          BoxShadow(
-            color: Theme.of(context).colorScheme.inverseSurface.withAlpha(20),
-            offset: const Offset(1, 1),
-            blurRadius: 4,
-          ),
-        ],
+    return AspectRatio(
+      aspectRatio: 2,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              // spreadRadius: 1,
+              color: Theme.of(context).colorScheme.inverseSurface.withAlpha(20),
+              offset: const Offset(-1, -1),
+              blurRadius: 4,
+            ),
+            BoxShadow(
+              color: Theme.of(context).colorScheme.inverseSurface.withAlpha(20),
+              offset: const Offset(1, 1),
+              blurRadius: 4,
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(0),
+        child: MaterialButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return const DeveloperInfoCard();
+                  });
+            },
+            padding: const EdgeInsets.all(0),
+            color: Theme.of(context).colorScheme.surface,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(7.0),
+              child: Image.asset('assets/images/logo.png'),
+            )),
       ),
-      padding: const EdgeInsets.all(0),
-      child: MaterialButton(
-          onPressed: () {
-            showDialog(
-                barrierDismissible: false,
-                context: context,
-                builder: (context) {
-                  return const DeveloperInfoCard();
-                });
-          },
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10.0),
-          color: Theme.of(context).colorScheme.surface,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Image.asset('assets/images/logo.png'),
-          )),
     );
   }
 }
