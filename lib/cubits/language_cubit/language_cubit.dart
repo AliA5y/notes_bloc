35a428b02d09ec8658d0ca74b8b888db50b7929a
AliA5y <ali.route9@gmail.com>
2024-10-05 07:35:26 +0300

    feature: add language cubit

---
+++ lib/cubits/language_cubit/language_cubit.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_bloc/shared.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'language_state.dart';
+
+class LanguageCubit extends Cubit<LanguageState> {
+  LanguageCubit()
+      : _language = 'ar',
+        super(LanguageInitial()) {
+    _initLanguage();
+  }
+  _initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  late SharedPreferences prefs;
+  String _language;
+
+  // bool get onBoarded {
+  //   return prefs.getBool(PrefsKeys.onboardingKey) ?? false;
+  // }
+
+  // bool get introduced {
+  //   return prefs.getBool(PrefsKeys.tourKey) ?? false;
+  // }
+
+  String get language => _language;
+  _initLanguage() async {
+    await _initPrefs();
+    emit(LanguageChangeLoading());
+    _language = prefs.getString(PrefsKeys.languageKey) ?? 'ar';
+    emit(LanguageChangeSuccess());
+  }
+
+  void changeLanguage(String languageLabel) async {
+    emit(LanguageChangeLoading());
+    prefs.setString(PrefsKeys.languageKey, languageLabel);
+    _language = languageLabel;
+    emit(LanguageChangeSuccess());
+  }
+
+  // void onBoard() async {
+  //   prefs.setBool(PrefsKeys.onboardingKey, true);
+  // }
+
+  // void setIntroduced() async {
+  //   prefs.setBool(PrefsKeys.tourKey, true);
+  // }
+}
